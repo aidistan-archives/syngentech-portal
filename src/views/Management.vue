98a8a464c0895300c8c3e3f5c1dadeea3cb63382
Aidi Stan <aidistan@live.com>
2019-07-09 18:55:13 +0800

    Add CVs of the management team

--- src/views/Management.vue
+++ src/views/Management.vue
@@ -3,52 +3,284 @@ en:
   cb:
     name: Bin Chen, PhD
     title: Chief Executive Officer
+    details: >
+      <ul>
+        <li>2018-2019, Head of Translational Sciences Asia (former Clinical Sciences China) in Bayer</li>
+        <li>2015-2018, Scientific Director & Early Development Scientific Leader of China Biologics Initiative in Janssen BioTherapeutics of Johnson & Johnson</li>
+        <li>2008-2015, Scientific Director of Global Clinical Pharmacology in Janssen R&D of Johnson & Johnson</li>
+        <li>2005-2008, Scientist and PKDM Lead in Amgen</li>
+        <li>2004-2005, Postdoctoral Fellow in College of Pharmacy at the Ohio State University</li>
+        <li>2000-2004, Master and Ph.D. in Biomedical Engineering at University of Nevada, Las Vegas</li>
+      </ul>
   jy:
     name: Yun Jiang, MBA
     title: Chief Operating Officer
+    details: >
+      <ul>
+        <li>Participate in the 4th Special Training Camp for CEO of Science and Technology Innovation, Ministry of Science and Technology Talent Center</li>
+        <li>Deputy Secretary of APALD-HCC</li>
+        <li>Participate in the 4th G20 Advanced Seminar of the Beijing Science and Technology Commission</li>
+      </ul>
+      <h3>CV Profile</h3>
+      <ul>
+        <li>Since 2014, Co-Founder, SyngenTech Co., Ltd.</li>
+        <li>2017-2019, UIBE-CTU MBA</li>
+        <li>2012-2014, National Laboratory of Information Science and Technology, Tsinghua University</li>
+      </ul>
   xz:
     name: Zhen Xie, PhD
     title: Chief Science Officer
+    details: >
+      <ul>
+        <li>National Special Advisor</li>
+        <li>High-End Leading Talents of Zhongguancun</li>
+        <li>Principle Investigator of Tsinghua Beijing National Research Center for Information Science and Technology of Center for Synthetic and Systems Biology of Automation in Tsinghua university</li>
+        <li>Member of Agricultural GMO Safety Committee in Ministry of Agriculture</li>
+        <li>Member of TR35 China science and Technology Young Heroes’ List committee in Massachusetts Institute of Technology (MIT)</li>
+      </ul>
+      <h3>CV Profile</h3>
+      <ul>
+        <li>Since 2012, Special Researcher, Tsinghua Beijing National Research Center for Information Science, Tsinghua University</li>
+        <li>Since 2012, Special Researcher, Technology of Center for Synthetic and Systems Biology of Automation, Tsinghua University</li>
+        <li>2010 - 2012, Postdoctoral Fellow, Computer Science and Artificial Intelligence Laboratory, Department of Biological Engineering, Massachusetts Institute of Technology (MIT), USA</li>
+        <li>2006 - 2010, Postdoctoral Fellow, FAS Center for Systems Biology, Harvard University, USA</li>
+        <li>2001- 2006, Doctor, University of Nevada, Las Vegas, USA</li>
+      </ul>
   lyy:
     name: Yinying Lu, PhD
     title: Chief Medical Officer
+    details: >
+      <ul>
+        <li>Director, Associate Professor, Associate Chief Physician and Master Tutor of Liver Cancer Diagnosis, Treatment and Research Center in 302 People's Liberation Army Hospital</li>
+        <li>Visiting Scholar at the National Cancer Institute (NCI)</li>
+        <li>Deputy Chairman of the Youth Committee in Beijing Hepatology Association</li>
+        <li>PI of the Synthesis and Systems Biology Center in Tsinghua University</li>
+        <li>Chairman of Asia-Pacific Alliance of Liver Diseases Academic Committee</li>
+        <li>Deputy Chairman of China Association of Research</li>
+        <li>Hospitals/Molecular Diagnostics Committee</li>
+        <li>Deputy Chairman of Hepatobiliary and Pancreatic Tumor Branch of Beijing Health Promotion Association</li>
+        <li>Secretary General of Overseas Returned Scholars Association and Youth Committee of the Physicians Association</li>
+        <li>Deputy Secretary-General of Overseas Returned Scholars Association and Chinese Scholars Association in NIH</li>
+        <li>Academic Committee Member Liver Cancer Branch and Tumor Immunology Branch of CSCO</li>
+      </ul>
+      <h3>CV Profile</h3>
+      <ul>
+        <li>2012-2013, Visiting Scholar, National Institutes of Health (NIH)</li>
+        <li>2010-2015, Deputy Director, Liver Cancer Diagnosis, Treatment and Research Center, 302 People's Liberation Army Hospital</li>
+        <li>2008-2010, Associate Chief Physician, 302 People's Liberation Army Hospital</li>
+      </ul>
   zxg:
     name: Xuegong Zhang, PhD
     title: Advisor
+    details: >
+      <ul>
+        <li>Director of Bioinformatics Department, National Laboratory of Information Science and Technology in Tsinghua University</li>
+        <li>Executive Director, Professor, Doctoral Supervisor of Synthesis and Systems Biology Center in Tsinghua University</li>
+        <li>The Unique Scientist in China of the Human Cell Atlas Project</li>
+        <li>Winner of the National Fund for Outstanding Youth</li>
+        <li>Chief Scientist of Bioinformatics 973</li>
+      </ul>
+      <h3>CV Profile</h3>
+      <ul>
+        <li>Since 2003, Director of Bioinformatics Department, National Laboratory of Information Science and Technology, Tsinghua University</li>
+        <li>Since 2002, Professor of Pattern Recognition and Bioinformatics, Department of Automation, Tsinghua University</li>
+        <li>Since 2002, Deputy Director, the Key Laboratory of Bioinformatics, Ministry of Education, Tsinghua University</li>
+        <li>From march to April 2007, Visiting Scholar, Department of Molecular and Computational Biology, University of Southern California</li>
+        <li>From February to March 2006, visited scientists of public health, Harvard University</li>
+        <li>2001-2002, Senior Visiting Scholar, Department of Biostatistics of Public Health, Harvard University</li>
+        <li>1999-2007, Director, Information Processing Institute, Department of Automation, Tsinghua University</li>
+        <li>1996-2002, Associate Professor, Pattern Recognition Theory and Application, Department of Automation, Tsinghua University</li>
+        <li>1994-1996, Lecturer, Department of Automation, Tsinghua University</li>
+      </ul>
   ls:
     name: Shao Li, PhD
     title: Advisor
+    details: >
+      <ul>
+        <li>Deputy Director, Professor, Doctoral Supervisor of Bioinformatics of National Laboratory of Information Science and Technology in Tsinghua University</li>
+        <li>Winner of the National Fund for Outstanding Youth</li>
+        <li>Winner of the Second Prize of National Science and Technology Progress Award</li>
+      </ul>
+      <h3>CV Profile</h3>
+      <ul>
+        <li>February 2006, Deputy Director, Department of Bioinformatics, National Laboratory of Information Science and Technology, Tsinghua University</li>
+        <li>2004-2009, Associate Researcher, Key Laboratory of Bioinformatics (Ministry of Education), Department of Automation, Tsinghua University</li>
+        <li>2003-2004, Lecture, Key Laboratory of Bioinformatics (Ministry of Education), Department of Automation, Tsinghua</li>
+        <li>2001-2003, Postdoctoral, Key Laboratory of Bioinformatics (Ministry of Education), Department of Automation, Tsinghua</li>
+      </ul>
   wxw:
     name: Xiaowo Wang, PhD
     title: Advisor
+    details: >
+      <ul>
+        <li>Associate Professor, Doctoral Supervisor</li>
+        <li>Department of Bioinformatics, National Laboratory of Information Science and Technology in Tsinghua University</li>
+      </ul>
+      <h3>CV Profile</h3>
+      <ul>
+        <li>Since 2016, Associate Professor (Tenure, PI, Doctoral Supervisor), Department of Automation, Tsinghua University</li>
+        <li>2012-2013, Visiting Scholar of Tang, University of California, Berkeley</li>
+        <li>2011- 2015, Associate Professor, Department of Automation, Tsinghua University</li>
+        <li>2008-2011, Lecturer, Department of Automation, Tsinghua University</li>
+      </ul>
   gj:
     name: Jin Gu, PhD
     title: Advisor
+    details: >
+      <ul>
+        <li>Associate Professor, Doctoral Supervisor</li>
+        <li>Department of Biological Informatics of National Laboratory of Information Science and Technology, Tsinghua University</li>
+      </ul>
+      <h3>CV Profile</h3>
+      <ul>
+        <li>Since 2017, Associate Professor and Doctoral Supervisor, Tsinghua University</li>
+        <li>2016-2017, Assistant Professor and Doctoral Supervisor, Tsinghua University</li>
+        <li>2009-2015, Lecturer, Tsinghua University</li>
+      </ul>
 zh:
   cb:
     name: 陈彬, PhD
     title: 首席执行官
+    details: >
+      <ul>
+        <li>曾任 德国拜耳公司 中国临床科学，主任</li>
+        <li>曾任 美国强生公司Janssen BioTherapeutics（JBIO），研发主任</li>
+      </ul>
+      <h3>履历简介</h3>
+      <ul>
+        <li>2018年~2019年，德国拜耳公司的中国临床科学中心，主任</li>
+        <li>2015年~2018年，美国强生公司JBIO研发主任，中国区负责人</li>
+        <li>2008年~2015年，强生药物研发部，总监/副总监 兼 临床药理带头人</li>
+        <li>2005年~2008年，美国安进公司药物研发部，科学带头人</li>
+        <li>2004年~2005年，俄亥俄州立大学药学院，博士后</li>
+        <li>2000年~2004年，内华达大学拉斯维加斯分校生物医学工程，博士</li>
+      </ul>
   jy:
     name: 蒋云, MBA
     title: 首席运营官
+    details: >
+      <ul>
+        <li>科技部人才中心科技创新CEO特训营，第四期</li>
+        <li>亚太肝病技术联盟肝癌专委会委员，副秘书长</li>
+        <li>北京市科委G20高级研修班，第四期</li>
+      </ul>
+      <h3>履历简介</h3>
+      <ul>
+        <li>2014年至今，北京合生基因科技有限公司，联合创始人</li>
+        <li>2017~2019年，UIBE-CTU MBA</li>
+        <li>2012年~2014年，清华大学信息科学与技术国家实验室</li>
+      </ul>
   xz:
     name: 谢震, PhD
     title: 首席科学家
+    details: >
+      <ul>
+        <li>国家特聘专家</li>
+        <li>中关村高端领军人才</li>
+        <li>清华大学北京信息科学与技术国家研究中心，研究员</li>
+        <li>清华大学合成与系统生物学中心，研究员</li>
+        <li>农业部农业转基因生物安全委员会，委员</li>
+        <li>麻省理工学院（MIT）TR35中国科技青年英雄榜评审委员会，委员</li>
+      </ul>
+      <h3>履历简介</h3>
+      <ul>
+        <li>2012年至今 ，清华大学北京信息科学与技术国家研究中心，特别研究员</li>
+        <li>2010年~2012年 ，美国麻省理工学院（MIT）生物工程系计算机科学与人工智能实验室，博士后</li>
+        <li>2006年~2010年，美国哈佛大学FAS系统生物学中心，博士后</li>
+        <li>2001年~2006年，美国内华达大学拉斯维加斯分校, 博士</li>
+      </ul>
   lyy:
     name: 陆荫英, PhD
     title: 首席医学官
+    details: >
+      <ul>
+        <li>解放军302医院肝脏肿瘤诊疗与研究中心主任，副教授，副主任医师硕士生导师</li>
+        <li>美国国家肿瘤研究院，访问学者</li>
+        <li>北京市肝病学会青委会，副主委</li>
+        <li>清华大学合成与系统生物中心PI</li>
+        <li>亚太肝病技术联盟肝癌学术委员会，主委</li>
+        <li>中国研究型医院协会/分子诊断专委会，副主委</li>
+        <li>北京市健康促进会/肝胆胰肿瘤分会，副主委</li>
+        <li>欧美同学会/医师协会青委会，秘书长</li>
+        <li>欧美同学会/NIH中国学者联谊会，副秘书长</li>
+        <li>CSCO 肝癌分会/肿瘤免疫学分会，学术委员</li>
+      </ul>
+      <h3>履历简介</h3>
+      <ul>
+        <li>2012年~2013年，美国国立卫生研究院，访问学者</li>
+        <li>2010年~2015年，解放军第302医院肝脏肿瘤诊疗与研究中心，副主任</li>
+        <li>2008年~2010年，解放军第302医院，副主任医师</li>
+      </ul>
   zxg:
     name: 张学工, PhD
     title: 顾问
+    details: >
+      <ul>
+        <li>教授，博士生导师</li>
+        <li>清华大学信息科学与技术国家实验室生物信息学部主任</li>
+        <li>清华大学合成与系统生物学中心执行主任</li>
+        <li>人类细胞图谱计划唯一中国科学家</li>
+        <li>国家杰出青年基金获得者</li>
+        <li>生物信息学973首席科学家</li>
+      </ul>
+      <h3>履历简介</h3>
+      <ul>
+        <li>2003年至今，清华大学信息科学与技术国家实验室生物信息学部，主任</li>
+        <li>2002年至今，清华大学自动化系 模式识别与生物信息学，教授</li>
+        <li>2002年至今，清华大学生物信息学教育部重点实验室，副主任</li>
+        <li>2007年3月~4月，南加州大学分子与计算生物学系，访问学者</li>
+        <li>2006年2月~3月，哈佛大学公共卫生学院，访问科学家</li>
+        <li>2001年~2002年，哈佛大学公共卫生学院生物统计系，高级访问学者</li>
+        <li>1999年~2007年，清华大学自动化系信息处理研究所，所长</li>
+        <li>1996年~2002年，清华大学自动化系 模式识别理论及应用，副教授</li>
+        <li>1994年~1996年，清华大学自动化系，讲师</li>
+      </ul>
   ls:
     name: 李梢, PhD
     title: 顾问
+    details: >
+      <ul>
+        <li>教授，博士生导师</li>
+        <li>清华大学信息科学与技术国家实验室生物信息学部副主任</li>
+        <li>国家杰出青年基金获得者</li>
+        <li>国家科技进步二等奖</li>
+      </ul>
+      <h3>履历简介</h3>
+      <ul>
+        <li>2006年2月至今，清华大学信息科学与技术国家实验室生物信息学部，副主任 </li>
+        <li>2004年~2009年，清华大学自动化系、生物信息学教育部重点实验室副，研究员</li>
+        <li>2003年~2004年，清华大学自动化系、生物信息学教育部重点实验室，讲师 </li>
+        <li>2001年~2003年，清华大学自动化系、生物信息学教育部重点实验室，博士后</li>
+      </ul>
   wxw:
     name: 汪小我, PhD
     title: 顾问
+    details: >
+      <ul>
+        <li>副教授，博士生导师</li>
+        <li>清华大学信息科学与技术国家实验室生物信息学部</li>
+      </ul>
+      <h3>履历简介</h3>
+      <ul>
+        <li>2016年至今，清华大学，自动化系，长聘副教授（Tenure，PI，博士生导师）</li>
+        <li>2012年~2013年，美国加州大学伯克利分校，唐氏访问学者</li>
+        <li>2011年~2015年，清华大学，自动化系，副教授</li>
+        <li>2008年~2011年，清华大学，自动化系，讲师</li>
+      </ul>
   gj:
     name: 古槿, PhD
     title: 顾问
+    details: >
+      <ul>
+        <li>副教授，博士生导师</li>
+        <li>清华大学信息科学与技术国家实验室生物信息学部</li>
+      </ul>
+      <h3>履历简介</h3>
+      <ul>
+        <li>2017年至今，清华大学 副教授、博导</li>
+        <li>2016年~ 2017年，清华大学 助理教授、博导</li>
+        <li>2009年~2015年，清华大学 讲师</li>
+      </ul>
 </i18n>
 
 <template>
@@ -57,49 +289,49 @@ zh:
 
     <div class="content">
       <el-row :gutter="20" type="flex" justify="space-around">
-        <el-col :xs="12" :sm="6">
-          <el-image :src="images[1]"></el-image>
+        <el-col :xs="12" :sm="6" @click.native="setDialog('cb')">
+          <el-image :src="images['cb']"></el-image>
           <div class="name">{{ $t('cb.name') }}</div>
           <div class="title">{{ $t('cb.title') }}</div>
         </el-col>
-        <el-col :xs="12" :sm="6">
-          <el-image :src="images[2]"></el-image>
+        <el-col :xs="12" :sm="6" @click.native="setDialog('jy')">
+          <el-image :src="images['jy']"></el-image>
           <div class="name">{{ $t('jy.name') }}</div>
           <div class="title">{{ $t('jy.title') }}</div>
         </el-col>
       </el-row>
 
       <el-row :gutter="20" type="flex" justify="space-around">
-        <el-col :xs="12" :sm="6">
-          <el-image :src="images[0]"></el-image>
+        <el-col :xs="12" :sm="6" @click.native="setDialog('xz')">
+          <el-image :src="images['xz']"></el-image>
           <div class="name">{{ $t('xz.name') }}</div>
           <div class="title">{{ $t('xz.title') }}</div>
         </el-col>
-        <el-col :xs="12" :sm="6">
-          <el-image :src="images[3]"></el-image>
+        <el-col :xs="12" :sm="6" @click.native="setDialog('lyy')">
+          <el-image :src="images['lyy']"></el-image>
           <div class="name">{{ $t('lyy.name') }}</div>
           <div class="title">{{ $t('lyy.title') }}</div>
         </el-col>
       </el-row>
 
       <el-row :gutter="10">
-        <el-col :xs="12" :sm="6">
-          <el-image :src="images[4]"></el-image>
+        <el-col :xs="12" :sm="6" @click.native="setDialog('zxg')">
+          <el-image :src="images['zxg']"></el-image>
           <div class="name">{{ $t('zxg.name') }}</div>
           <div class="title">{{ $t('zxg.title') }}</div>
         </el-col>
-        <el-col :xs="12" :sm="6">
-          <el-image :src="images[5]"></el-image>
+        <el-col :xs="12" :sm="6" @click.native="setDialog('ls')">
+          <el-image :src="images['ls']"></el-image>
           <div class="name">{{ $t('ls.name') }}</div>
           <div class="title">{{ $t('ls.title') }}</div>
         </el-col>
-        <el-col :xs="12" :sm="6">
-          <el-image :src="images[6]"></el-image>
+        <el-col :xs="12" :sm="6" @click.native="setDialog('wxw')">
+          <el-image :src="images['wxw']"></el-image>
           <div class="name">{{ $t('wxw.name') }}</div>
           <div class="title">{{ $t('wxw.title') }}</div>
         </el-col>
-        <el-col :xs="12" :sm="6">
-          <el-image :src="images[7]"></el-image>
+        <el-col :xs="12" :sm="6" @click.native="setDialog('gj')">
+          <el-image :src="images['gj']"></el-image>
           <div class="name">{{ $t('gj.name') }}</div>
           <div class="title">{{ $t('gj.title') }}</div>
         </el-col>
@@ -107,6 +339,15 @@ zh:
 
       <Footer></Footer>
     </div>
+
+    <el-dialog :visible="dialog !== null" :before-close="closeDialog">
+      <div class="dialog-header">
+        <el-image :src="images[dialog]"></el-image>
+        <h1>{{ $t(`${dialog}.name`) }}</h1>
+        <h2>{{ $t(`${dialog}.title`) }}</h2>
+      </div>
+      <div class="dialog-content" v-html="$t(`${dialog}.details`)"></div>
+    </el-dialog>
   </div>
 </template>
 
@@ -118,16 +359,17 @@ export default {
   data () {
     return {
       banner: require('../assets/banner-3.jpg'),
-      images: [
-        require('../assets/高管-谢震.png'),
-        require('../assets/高管-陈彬.png'),
-        require('../assets/高管-蒋云.png'),
-        require('../assets/高管-陆荫英.png'),
-        require('../assets/顾问-张学工.png'),
-        require('../assets/顾问-李梢.png'),
-        require('../assets/顾问-汪小我.png'),
-        require('../assets/顾问-古槿.png')
-      ]
+      images: {
+        cb: require('../assets/高管-陈彬.png'),
+        jy: require('../assets/高管-蒋云.png'),
+        xz: require('../assets/高管-谢震.png'),
+        lyy: require('../assets/高管-陆荫英.png'),
+        zxg: require('../assets/顾问-张学工.png'),
+        ls: require('../assets/顾问-李梢.png'),
+        wxw: require('../assets/顾问-汪小我.png'),
+        gj: require('../assets/顾问-古槿.png')
+      },
+      dialog: null
     }
   },
   computed: {
@@ -143,6 +385,10 @@ export default {
     '$i18n.locale' () {
       document.title = this.title
     }
+  },
+  methods: {
+    setDialog (name) { this.dialog = name },
+    closeDialog () { this.dialog = null }
   },
   components: { Footer }
 }
@@ -171,5 +417,23 @@ export default {
   .name, .title {
     text-align: center;
   }
+}
+
+#management .el-dialog {
+  .dialog-header {
+    text-align: center;
+    margin-bottom: 40px;
+
+    .el-image { max-width: 200px; }
+  }
+
+  .dialog-content {
+    li {
+      word-break: keep-all;
+      word-wrap: break-word;
+    }
+  }
+
+  @media screen and (max-width: 600px) { & { width: 80%; } }
 }
 </style>
