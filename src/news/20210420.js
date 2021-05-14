ef1374ae3126f78e249c2c3af4f52e7c5067656d
Aidi Stan <aidistan@live.com>
2021-05-14 14:01:00 +0800

    Add @/news/20210420

---
+++ src/news/20210420.js
@@ -0,0 +1,48 @@
+import image1 from '@/assets/news/20210420_1.jpg'
+
+export default {
+  en: {
+    title: 'The preclinical data of our first oncolytic virus product SynOV1.1 was released at the AACR annual meeting 2021',
+    abstract: 'Beijing SyngenTech Co., LTD. (SyngenTech) is the first high-tech company dedicated to applying synthetic biotechnology in the field of biomedicine in China. SyngenTech recently released the preclinical data of its first oncolytic virus product, SynOV1.1, at this year\'s American Association for Cancer Research (AACR) annual meeting.',
+    cover: image1,
+    content: `
+  <div style="text-align:center"><img src="${image1}"></img></div>
+  <p>Beijing SyngenTech Co., LTD. (SyngenTech) is the first high-tech company dedicated to applying synthetic biotechnology in the field of biomedicine in China. SyngenTech recently released the preclinical data of its first oncolytic virus product, SynOV1.1, at this year's American Association for Cancer Research (AACR) annual meeting.</p>
+  <p>Currently, the poster titled “SynOV1.1: A Synthetic Gene Circuit Controlled Oncolytic Adenovirus Demonstrating High Tumor Specificity, Potent Antitumor Efficacy and High Synergy with An Anti-PD-L1 Monoclonal Antibody in Preclinical Hepatocellular Carcinoma Models” can be accessed through the AACR official website.</p>
+  <p>The poster abstract is summarized as follows:</p>
+  <ul>
+    <li>SynOV1.1 is precisely regulated by synthetic gene circuits, which can significantly improve its specificity for tumor cell killing and produce local and system immune responses.</li>
+    <li>Compared with sorafenib, SynOV1.1 has a stronger inhibitory effect on the growth of human liver cancer transplanted tumors in nude mice, and significantly prolongs the survival time of mice.</li>
+    <li>In the tumor-bearing immune-competent mice, SynOV1.1m (SynOV1.1 mouse surrogate) demonstrated systemic abscopal effect in the non-injected contralateral tumors. </li>
+    <li>In the tumor-bearing immune-competent mice, the anti-tumor effect of SynOV1.1m combined with anti-PD-L1 antibody is better than that of the single-agent treatment group, showing a better synergistic effect.</li>
+    <li>Preclinical safety of SynOV1.1 was examined in various toxicology species including golden hamsters, tumor-bearing mice and cynomolgus monkey, in all of which SynOV1.1 was found safe. </li>
+  </ul>
+  <h3>About SynOV1.1</h3>
+  <p>SynOV is an oncolytic adenovirus therapy platform developed by SyngenTech for logical gene circuit-based regulation. Its first product, SynOV1.1, is a gene therapy product targeting alpha-fetoprotein positive solid tumour. This innovative drug is designed with the idea of synthetic biology and uses artificial gene circuits to identify multiple biomarkers in tumour cells, regulate their targeting tumour specificity, secrete immune factors to stimulate the anti-tumour immune response and improve tumour killing ability and clinical application safety. The product received U.S. Food and Drug Administration (FDA) approval for clinical trial application on November 27, 2020.</p>
+  <h3>About SyngenTech</h3>
+  <p>SyngenTech is a domestic leading national high-tech enterprise, which is dedicated to the application of synthetic biology in the fields of biomedicine and life health. SyngenTech is focuses on the research and development of gene and cell therapeutics based on synthetic biology. It aims to bring this innovative drug with the great clinical potential to the masses of patients as soon as possible.</p>
+    `
+  },
+  zh: {
+    title: '合生基因在2021年AACR年会上公布其第一个溶瘤病毒产品SynOV1.1的临床前数据',
+    abstract: '北京合生基因科技有限公司是中国首家致力于合成生物技术在生物医药领域应用的高新技术公司。合生基因近日在今年的美国癌症研究协会（AACR）年会上公布其第一个溶瘤病毒产品SynOV1.1的临床前数据。',
+    cover: image1,
+    content: `
+  <div style="text-align:center"><img src="${image1}"></img></div>
+  <p>北京合生基因科技有限公司（以下简称合生基因）是中国首家致力于合成生物技术在生物医药领域应用的高新技术公司。合生基因近日在今年的美国癌症研究协会（AACR）年会上公布其第一个溶瘤病毒产品SynOV1.1的临床前数据。</p>
+  <p>目前，可通过AACR官网访问标题为“SynOV1.1: A Synthetic Gene Circuit Controlled Oncolytic Adenovirus Demonstrating High Tumor Specificity, Potent Antitumor Efficacy and High Synergy with An Anti-PD-L1 Monoclonal Antibody in Preclinical Hepatocellular Carcinoma Models”的壁报。</p>
+  <p>壁报摘要如下：</p>
+  <ul>
+    <li>SynOV1.1受合成基因线路精准调控，能够显著提高其对肿瘤细胞杀伤的特异性，并产生局部及周身免疫反应。</li>
+    <li>相较索拉非尼，SynOV1.1对裸鼠人源肝癌移植瘤具有更强的抑制生长效果，并显著延长小鼠生存期。</li>
+    <li>在免疫健全小鼠荷瘤模型上，SynOV1.1m（SynOV1.1鼠源替代物）能激发系统性免疫反应，显著抑制注射侧和远端肿瘤的生长。</li>
+    <li>在免疫健全小鼠荷瘤模型上，SynOV1.1m与抗PD-L1抗体联用抑瘤效果优于单药治疗组，体现较好的协同作用。</li>
+    <li>在开展的多个重复给药毒理试验中（包括：荷瘤小鼠、金黄地鼠、食蟹猴毒理试验），SynOV1.1具有较好的安全性</li>
+  </ul>
+  <h3>关于SynOV1.1</h3>
+  <p>SynOV1.1是合生基因基于新型溶瘤病毒基因治疗药物平台SynOV系统开发的第一款产品，由甲胎蛋白（AFP）为启动子的高靶向特异性基因治疗溶瘤腺病毒，主要针对 AFP 阳性的实体瘤患者。临床前研究显示，SynOV1.1显著提高了对肝细胞肝癌的治疗效果和安全性，表明其具有极强的临床应用价值。该产品已于2020年11月获得美国FDA临床试验许可，同时在中国的临床试验申请正在进行中。</p>
+  <h3>关于合生基因</h3>
+  <p>合生基因作为中国首家致力于合成生物技术在生物医药领域应用的高新技术企业，于2014年成立于中关村生命科学园。合生基因期望利用合成生物技术开发针对癌症、遗传病、传染病治疗的创新药物和治疗方法，构建基于合成生物技术的生物医药新生态。通过合理设计构建新型基因、免疫细胞和RNA核酸药物，提高药物研发成功率、有效性和安全性。值得关注的是，合生基因也在加码布局以合成生物技术为基础的下一代生物药物智能制造平台开发。</p>
+    `
+  }
+}
